11d9020243680dd650aec8e7275b772057a59877
DJ1209 <59895036+DJ1209@users.noreply.github.com>
2020-04-25 14:44:56 -0500

    deploy it

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
     password: "Disha@1234",
     database: "burger_db"
