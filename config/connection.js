35cfd45645d2359c4433ccb7de2b761bc89d2511
DJ1209 <59895036+DJ1209@users.noreply.github.com>
2020-05-04 13:26:42 -0500

    Deploy it

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) {
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Disha@1234",
-    database: "burger_db"
-});
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Disha@1234',
+        database: 'burger_db'
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
